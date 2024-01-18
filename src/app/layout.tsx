614b4d9f2dba3b7a2d363075e3e202f97b692772
mharry18 <matthew.s.harry11@gmail.com>
2024-01-18 13:37:08 -0600

    Fix font issue in navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+// import { Inter } from 'next/font/google';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
 import Footer from './footer';
 import MainNavBar from '@/navbar';
+import './page.css';
 // import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -23,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <MainNavBar />
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
       <Footer />
     </html>
   );
